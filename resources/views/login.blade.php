09a2602b1b1e5bd713932c8ba749716fb1bee8f3
MeysaRP <meysaramelia@gmail.com>
2026-03-13 09:23:06 +0700

    setup MVC klinik digital Login (Meysa)

---
+++ resources/views/login.blade.php
@@ -0,0 +1,11 @@
+<h1>Login Klinik Digital</h1>
+
+<form>
+    <label>Username</label><br>
+    <input type="text"><br><br>
+
+    <label>Password</label><br>
+    <input type="password"><br><br>
+
+    <button type="submit">Login</button>
+</form>
