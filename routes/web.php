9ef2c24b01484e5ab75bf6e4470aeadb6e6bbb62
anggunbatubara2007-bit <anggunbatubara2007@gmail.com>
2026-04-06 19:01:45 +0700

    menambahkan route homepage dan registrasi pada file web

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PasienController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Dashboard_pasienController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/login', [LoginController::class, 'index']);
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard_pasien', [DashboardController::class, 'index']);
 Route::get('/pasien', [PasienController::class, 'index']);
+Route::view('/home', 'Homepage');
+Route::view('/register', 'Registrasi');
