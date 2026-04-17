200c4ded351dab549378857ba073f8b868168c93
MeysaRP <meysaramelia@gmail.com>
2026-04-17 19:31:44 +0700

    Upgrade

--- resources/views/layouts/dashboard_pasien.blade.php
+++ resources/views/layouts/dashboard_pasien.blade.php
@@ -43,7 +43,7 @@
                 <li>
                     <a href="#" class="flex items-center p-3 text-base font-medium text-gray-700 rounded-xl hover:bg-gray-100 group">
                         <svg class="w-5 h-5 mr-3 text-gray-400 group-hover:text-[#09637E]" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
-                        Jadwal Booking
+                        Jadwal Pemeriksaan
                     </a>
                 </li>
                 <li>
