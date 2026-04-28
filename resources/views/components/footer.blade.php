737f819ca22e3f24373b3e4a3e3d1fe1d3d68d89
MeysaRP <meysaramelia@gmail.com>
2026-04-28 12:05:49 +0700

    Upgrade

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,3 @@
-<footer class="bg-[#09637E] mt-10">
-    <div class="max-w-screen-xl mx-auto p-6 md:py-6">
-        <hr class="my-4 border-white/20 sm:mx-auto lg:my-4" />
-        <span class="block text-sm text-white/50 sm:text-center">©️ 2026 Politeknik Negeri Batam - Projek PBL
-            IFpagi2A-02. All Rights Reserved.</span>
-    </div>
+<footer class="bg-[#09637E] p-4 text-center">
+    <span class="text-xs text-white/70">© 2026 Politeknik Negeri Batam - Projek PBL IFpagi2A-02</span>
 </footer>
